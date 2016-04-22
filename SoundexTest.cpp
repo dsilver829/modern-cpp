984089ab22294a530f6b69db953025178346e71d
U-FORDNA2\dsilve11 <dsilve11@WGC1W7DJX1N32.srl.ford.com>
2016-04-22 15:20:02 -0700

    finish the first test

--- SoundexTest.cpp
+++ SoundexTest.cpp
@@ -1,8 +1,18 @@
+#include <string>
+
 class Soundex {
+  public:
+    std::string encode(const std::string& word) const {
+      return "A";
+    }
 };
 
 #include "gmock/gmock.h"
 
 TEST(SoundexEncoding, RetainSoleLetterOfOneLetterWord) {
   Soundex soundex;
+
+  auto encoded = soundex.encode("A");
+
+  ASSERT_THAT(encoded, testing::Eq("A"));
 }
