1885abac6803fe4c5fb0068963ac5a7435c6810a
David Silver <dsilver@candidatemetrics.com>
2016-04-25 12:33:14 -0700

    fix spec

--- Soundex.h
+++ Soundex.h
@@ -23,15 +23,14 @@ class Soundex {
 
     std::string encodedDigit(char letter) const {
       const std::unordered_map<char, std::string> encodings {
-        {'b', "1"},
-        {'c', "2"},
-        {'d', "3"}
+        {'b', "1"}, {'f', "1"}, {'p', "1"}, {'v', "1"},
+        {'c', "2"}, {'g', "2"}, {'j', "2"}, {'k', "2"}, {'q', "2"}, {'s', "2"}, {'x', "2"}, {'z', "2"},
+        {'d', "3"}, {'t', "3"},
+        {'l', "4"},
+        {'m', "5"}, {'n', "5"},
+        {'r', "6"}
       };
-      encodings.find(letter)->second;
-    }
-
-    std::string encodedDigit() const {
-      return "1";
+      return encodings.find(letter)->second;
     }
 
     std::string zeroPad(const std::string& word) const {
