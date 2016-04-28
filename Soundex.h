3d1eb102170970400e036740d9445ff3d0fc30cd
David Silver <dsilver@candidatemetrics.com>
2016-04-28 09:14:54 -0700

    don't duplicate consecutive character encodings

--- Soundex.h
+++ Soundex.h
@@ -11,20 +11,6 @@ class Soundex {
       return zeroPad(head(word) + encodedDigits(tail(word)));
     }
 
-  private:
-    std::string head(const std::string& word) const {
-      return word.substr(0,1);
-    }
-
-    std::string encodedDigits(const std::string& word) const {
-      std::string encoding;
-      for(auto letter: word) {
-        if(isComplete(encoding)) break;
-        encoding += encodedDigit(letter);
-      }
-      return encoding;
-    }
-
     std::string encodedDigit(char letter) const {
       const std::unordered_map<char, std::string> encodings {
         {'b', "1"}, {'f', "1"}, {'p', "1"}, {'v', "1"},
@@ -38,6 +24,22 @@ class Soundex {
       return it == encodings.end() ? "" : it->second;
     }
 
+  private:
+    std::string head(const std::string& word) const {
+      return word.substr(0,1);
+    }
+
+    std::string encodedDigits(const std::string& word) const {
+      std::string encoding;
+      for(auto letter: word) {
+        if(isComplete(encoding)) break;
+        if(encodedDigit(letter) != lastDigit(encoding)) {
+          encoding += encodedDigit(letter);
+        }
+      }
+      return encoding;
+    }
+
     std::string zeroPad(const std::string& word) const {
       auto zerosNeeded = MaxCodeLength - word.length();
       return word + std::string(zerosNeeded, '0');
@@ -49,6 +51,11 @@ class Soundex {
 
     bool isComplete(const std::string& encoding) const {
       return encoding.length() == MaxCodeLength - 1;
+    }
+
+    std::string lastDigit(const std::string& encoding) const {
+			if(encoding.empty()) return "";
+      return std::string(1, encoding.back());
     }
 };
 
